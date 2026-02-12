e94143497b8f25621d87c5f20f0a1835b607a552
sxd0 <fancityfun@gmail.com>
2026-02-13 00:30:27 +0300

    feat: add new fixtures for register

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,12 +2,29 @@ import pytest
 from src.database import Base, engine_null_pool
 from src.models import *
 from src.config import settings
+from httpx import ASGITransport, AsyncClient
+from src.main import app
+
+
+@pytest.fixture(scope="session", autouse=True)
+def check_test_mode():
+    assert settings.MODE == "TEST"
 
 
 @pytest.fixture(scope="session", autouse=True)
 async def async_main():
-    assert settings.MODE == "TEST"
-
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def register_user():
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        await ac.post(
+            "/auth/register",
+            json={
+                "email": "kot@pes.com",
+                "password": "1234",
+            }
+        )
--- tests/integration_tests/hotels/test_db.py
+++ tests/integration_tests/hotels/test_db.py
@@ -1,11 +1,11 @@
 from src.schemas.hotels import HotelAdd
 from src.utils.db_manager import DBManager
-from src.database import async_session_maker
+from src.database import async_session_maker_null_pool
 
 
 async def test_add_hotel():
     hotel_data = HotelAdd(title="Hotel 5 start", location="Sochi")
-    async with DBManager(session_factory=async_session_maker) as db:
+    async with DBManager(session_factory=async_session_maker_null_pool) as db:
         new_hotel_data = await db.hotels.add(hotel_data)
         await db.commit()
         print(f"{new_hotel_data=}")
